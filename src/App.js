5985f344edd0c2ba3c1257451a37bf395f258485
outta057 <outta057@gmail.com>
2025-12-26 20:22:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+import "./scss/app.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<Header />
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+						<Sort />
+					</div>
+
+					<h2 className="content__title">Все пиццы</h2>
+
+					<div className="content__items">
+						<PizzaBlock title = 'Meksikana' price = {500} />
+						<PizzaBlock title = 'Banana' price = {350}/>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
