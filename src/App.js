ce0483f3f646940ca4eeec6fcab9d6f1425c6829
outta057 <outta057@gmail.com>
2026-02-10 01:34:21 +0200

    добавил редакс тулкит тестовым счётчиком

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 // import pizzas from "./assets/pizzas.json";
 import React from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
+
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+
 import "./scss/app.scss";
 
 export const SearchContext = React.createContext();
 
 function App() {
 	const [searchValue, setSearchValue] = React.useState("");
+	const count = useSelector(state => state.counter.count);
+	const dispatch = useDispatch();
 
 	return (
 		<div className="wrapper">
+			<button
+				aria-label="Increment value"
+				onClick={() => dispatch(increment())}
+			>
+				Increment
+			</button>
+			<span>{count}</span>
+			<button
+				aria-label="Decrement value"
+				onClick={() => dispatch(decrement())}
+			>
+				Decrement
+			</button>
+
 			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
 				<Header />
 				<div className="content">
 					<Routes>
-						<Route path="/" element={<Home  />} />
+						<Route path="/" element={<Home />} />
 						<Route path="/cart" element={<Cart />} />
 
 						<Route path="/*" element={<NotFound />} />
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter} from "react-router-dom";
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from './redux/store'
+import {Provider} from 'react-redux'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
+	<BrowserRouter>
+		<Provider store={store}>
+		  <App />
+        </Provider>
+	</BrowserRouter>,
 );
-
-
