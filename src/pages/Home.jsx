fcc2ee018f94b6b2658ab83de53285d76728afd1
outta057 <outta057@gmail.com>
2026-02-20 02:02:28 +0200

    добавил асинк евейт на фетч запрос

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
 		dispatch(setCurrentPage(number));
 	};
 
-	const fetchPizzas = () => {
+	const fetchPizzas = async () => {
 		setIsLoading(true);
 
 		const sortBy = sort.sortProperty.replace("-", "");
@@ -44,17 +44,23 @@ const Home = () => {
 		const category = categoryId > 0 ? `category=${categoryId}` : "";
 		const search = searchValue ? `&search=${searchValue}` : "";
 
-		axios
-			.get(
+		try {
+			const res = await axios.get(
 				`https://69750e05265838bbea969def.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-			)
-			.then(res => {
-				setItems(res.data);
-				setIsLoading(false);
-			});
+			);
+
+			setItems(res.data);
+			setIsLoading(false);
+		} catch (error) {
+			setIsLoading(false);
+			console.log("ERROR", error);
+		} finally {
+			setIsLoading(false);
+		}
+
+		window.scrollTo(0, 0);
 	};
 
-	
 	// если изменили параметры и был первый рендер
 	React.useEffect(() => {
 		if (isMounted.current) {
@@ -73,9 +79,9 @@ const Home = () => {
 		if (window.location.search) {
 			const params = qs.parse(window.location.search.substring(1));
 
-			const sort = sortList.find(
-				obj => obj.sortProperty === params.sortProperty,
-			) || sortList[0];
+			const sort =
+				sortList.find(obj => obj.sortProperty === params.sortProperty) ||
+				sortList[0];
 
 			dispatch(
 				setFilters({
@@ -88,7 +94,6 @@ const Home = () => {
 		}
 	}, []);
 
-
 	// если был первый рендер, то запрашиваем пиццы
 	React.useEffect(() => {
 		window.scrollTo(0, 0);
@@ -99,8 +104,6 @@ const Home = () => {
 
 		isSearch.current = false;
 	}, [categoryId, sort.sortProperty, searchValue, currentPage]);
-
-	
 
 	const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />);
 
